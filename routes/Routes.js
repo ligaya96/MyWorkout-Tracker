beaa46b92277227c8841d934b8b037a7c556e128
ligaya <ligaya96@gmail.com>
2021-04-21 22:25:02 -0700

    finshed routes

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,4 +1,56 @@
 //made a route folder 
 // Routes 
+const express = require("express");
+const router = express.Router();
+const Excerise = require("../models/workoutSchema");
+
+//Get Workouts
+router.get("/api/workouts", (req, res) => {
+    Excerise.find()
+    .sort({date: -1})
+    .then(dbWorkouts =>{
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+})
+
+//Post certain workouts
+router.post("/api/workouts", ({body}, res) => {
+    Excerise.create(body)
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err =>{
+        res.status(400).json(err);
+    })
+})
+
+// Put 
+router.put("/api/workouts/:id", ({ body, params}, res) => {
+ Excerise.findByIdAndUpdate(
+     params.id, {$push: {exercises : body }
+     },
+     {
+         new :true,
+     }
+ ).then(dbWorkouts => {
+     res.json(dbWorkouts);
+ }).catch(err => {
+    res.status(400).json(err);
+ });
+});
+// Get 
+router.get("/api/workouts/range", (req, res) => {
+    Excerise.find({})
+    .sort({date: -1})
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+})
 
 
